9c8d3c39047dddcda4893384df3e50cacb658986
Jakub Semecky <jakub.semecky@hogarth.com>
2025-02-14 10:39:55 +0100

    reset changes

--- commitlint.config.js
+++
@@ -1,6 +0,0 @@
-// Commit message should be "<type>: <subject>", all lowercase
-// Types example: 'feat', 'fix', 'chore'
-// Example commit: "feat: create configuration for commitlint"
-module.exports = {
-    extends: ['@commitlint/config-conventional']
-};
